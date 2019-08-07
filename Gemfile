765886583e4af968f11dfcc7f15b87be27567eb1
Seth NeJame <sethnejame@gmail.com>
2019-08-07 10:30:05 +0200

    Migrating DB, new User model created, error w/ mount_devise

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'rack-cors', require: 'rack/cors'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'devise_token_auth'
+# gem 'devise'
 
 group :development, :test do
   gem 'pry-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  # mount_devise_token_auth_for 'User', at: 'auth'
+
   namespace :api do
     namespace :v0 do
       resources :pings, only: [:index], constraints: { format: 'json' }
