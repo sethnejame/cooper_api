49292cbe8bcedfd9e67f9464c00549381fb4bced
Seth NeJame <sethnejame@gmail.com>
2019-08-07 10:54:11 +0200

    Adding more  unit tests for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,43 @@ RSpec.describe User, type: :model do
   it "should have a valid Factory" do
     expect(create(:user)).to be_valid
   end
+
+  describe 'Database table' do
+    it { is_expected.to have_db_column :id }
+    it { is_expected.to have_db_column :provider }
+    it { is_expected.to have_db_column :uid }
+    it { is_expected.to have_db_column :encrypted_password }
+    it { is_expected.to have_db_column :reset_password_token }
+    it { is_expected.to have_db_column :reset_password_sent_at }
+    it { is_expected.to have_db_column :remember_created_at }
+    it { is_expected.to have_db_column :confirmed_at}
+    it { is_expected.to have_db_column :confirmation_token }
+    it { is_expected.to have_db_column :confirmation_sent_at }
+    it { is_expected.to have_db_column :unconfirmed_email }
+    it { is_expected.to have_db_column :nickname }
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to have_db_column :image }
+    it { is_expected.to have_db_column :nickname }
+    it { is_expected.to have_db_column :tokens }
+    it { is_expected.to have_db_column :created_at }
+    it { is_expected.to have_db_column :updated_at }
+  end
+
+  context 'should not have an invalid email address' do
+      emails= ['asdf@ ds.com', '@example.com', 'test me @yahoo.com',
+                'asdf@example', 'ddd@.d. .d', 'ddd@.d']
+      
+      emails.each do |email|
+        if { is_expdcted.not_to allow_value(email).for(:email) }
+      end
+  end
+
+  context 'should have an invalid email address' do
+    emails= ['asdf@ds.com', 'yes@example.com', 'testme@yahoo.com',
+              'asDf@example.org', 'ddd@.de', 'dd.dd@whatever.yo']
+    
+    emails.each do |email|
+      if { is_expdcted.not_to allow_value(email).for(:email) }
+    end
+end
 end
