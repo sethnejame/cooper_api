04431113c7ebda1e661900c294c782da81328628
Seth NeJame <sethnejame@gmail.com>
2019-08-07 14:56:36 +0200

    Fixing new migration to fix test (valid creds returns user)

--- db/migrate/20190807125011_add_trackable_columns_to_user.rb
+++ db/migrate/20190807125011_add_trackable_columns_to_user.rb
@@ -1,12 +1,10 @@
 class AddTrackableColumnsToUser < ActiveRecord::Migration[5.2]
   def change
-    change_table :users do |t|
-      ## Trackable
-      t.add_column :sign_in_count, :integer, :default => 0
-      t.add_column :current_sign_in_at, :datetime
-      t.add_column :last_sign_in_at, :datetime
-      t.add_column :current_sign_in_ip, :string
-      t.add_column :last_sign_in_ip, :string
+      add_column :users, :sign_in_count, :integer, :default => 0
+      add_column :users, :current_sign_in_at, :datetime
+      add_column :users,:last_sign_in_at, :datetime
+      add_column :users,:current_sign_in_ip, :string
+      add_column :users,:last_sign_in_ip, :string
     end
   end
 end
