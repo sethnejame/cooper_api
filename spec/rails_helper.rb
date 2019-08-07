29fe5d710b6b7a0030bb1db06ed712bf8d0e1964
Seth NeJame <sethnejame@gmail.com>
2019-08-07 09:40:54 +0200

    Adding namespace/routes to main config for api

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,7 @@ require 'spec_helper'
 
 ActiveRecord::Migration.maintain_test_schema!
 
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require F }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
